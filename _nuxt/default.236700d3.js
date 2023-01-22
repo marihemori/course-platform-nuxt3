2e744c8dc8ace16040f63d8140b366f647502ff9
Mariana <marianahemorais@gmail.com>
2023-01-22 17:53:49 -0300

    6b5b07d

---
+++ _nuxt/default.236700d3.js
@@ -0,0 +1,1 @@
+import{i as s,b as t,E as c,o as l}from"./entry.9ba8f686.js";const o={},r={class:"p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"};function n(e,a){return l(),t("div",r,[c(e.$slots,"default")])}const _=s(o,[["render",n]]);export{_ as default};
---
+++ _nuxt/lesson.cd28f061.js
@@ -0,0 +1,1 @@
+import{i as e,b as n,o}from"./entry.9ba8f686.js";const s={};function c(r,t){return o(),n("div",null," A lesson here ")}const l=e(s,[["render",c]]);export{l as default};
